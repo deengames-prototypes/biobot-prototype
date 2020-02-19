6a909dbee99aae7c8b984c679ff5f3bcbaab31ba
nightblade9 <nightbladecodes@gmail.com>
2020-02-19 10:35:30 -0500

    Configure bow/forest. Fix some crashes (on fire).

--- view/targeting_monster.py
+++ view/targeting_monster.py
@@ -3,10 +3,10 @@ def closest_monster(game, max_range):
     closest_enemy = None
     closest_dist = max_range + 1  # start with (slightly more than) maximum range
 
-    for obj in game.area_map.entities:
-        if game.fighter_system.has(obj) and game.fighter_system.get(obj).hostile and (obj.x, obj.y) in game.renderer.visible_tiles:
+    for obj in game.instance.area_map.entities:
+        if game.instance.fighter_system.has(obj) and game.instance.fighter_system.get(obj).hostile and (obj.x, obj.y) in game.instance.renderer.visible_tiles:
             # calculate distance between this object and the player
-            dist = game.player.distance_to(obj)
+            dist = game.instance.player.distance_to(obj)
             if dist < closest_dist:  # it's closer, so remember it
                 closest_enemy = obj
                 closest_dist = dist
