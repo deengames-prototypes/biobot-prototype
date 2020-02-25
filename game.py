742c531e72c5592eaa484591ac591bae56b813f7
nightblade9 <nightbladecodes@gmail.com>
2020-02-25 10:33:12 -0500

    Map size scales up/down to difficulty

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, MAP_WIDTH, MAP_HEIGHT, PANEL_HEIGHT, LIMIT_FPS
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, MAP_WIDTH, MAP_HEIGHT, PANEL_HEIGHT, LIMIT_FPS, BASE_DIFFICULTY, DIFFICULTY_PER_COLUMN_INCREASE, DIFFICULTY_PER_ROW_INCREASE
 from model.config import config
 from model.event.event_bus import EventBus
 from model.systems.system import ComponentSystem
@@ -45,8 +45,15 @@ class Game:
         from model.maps import generators
         from model.maps.area_map import AreaMap
         from difficulty import Difficulty
+        
+        # Scale map with difficulty
+        current_difficulty = Difficulty.instance.current_difficulty
+        extra_rows = (current_difficulty - BASE_DIFFICULTY) // DIFFICULTY_PER_ROW_INCREASE
+        extra_columns = (current_difficulty - BASE_DIFFICULTY) // DIFFICULTY_PER_COLUMN_INCREASE
+        map_width = MAP_WIDTH + extra_rows
+        map_height = MAP_HEIGHT + extra_columns
 
-        self.area_map = AreaMap(MAP_WIDTH, MAP_HEIGHT, self.current_floor)
+        self.area_map = AreaMap(map_width, map_height, self.current_floor)
         self.event_bus = EventBus()
         Difficulty() # re-inits watch events, nothing lost
 
